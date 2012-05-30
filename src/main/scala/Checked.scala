ae5ed896bb9e61f0641105ef815cc440973b9ec3
Rob Dickens <rob.dickens@lafros.com>
2012-05-30 21:00:35 +0100

    S(String)=>R(Reason); +get +reason
    -sealed abstract class Checked[+A, +S] {
    +sealed abstract class Checked[+A, +R] {
    
    +  def get: A = this match {
    
    +  def reason: R = this match {
    
    -  def map[B](f: A => B): Checked[B, S] = this match {
    +  def map[B](f: A => B): Checked[B, R] = this match {
    
    -    case Kayo(b) => Kayo(b)
    +    case Kayo(r) => Kayo(r)
    
    -  def flatMap[B, T >: S](f: A => Checked[B, T]): Checked[B, T] = this match {
    +  def flatMap[B, S >: R](f: A => Checked[B, S]): Checked[B, S] = this match {
    
    -    case Kayo(b) => Kayo(b)
    +    case Kayo(s) => Kayo(s)
    
    -  def withFilter(p: A => Boolean): Checked[A, S] = this match {
    +  def withFilter(p: A => Boolean): Checked[A, R] = this match {
    
    -    case kayo @ Kayo(b) => kayo
    +    case kayo @ Kayo(_) => kayo
    
    -final case class Okay[+A, +S](a: A) extends Checked[A, S] {
    +final case class Okay[+A, +R](a: A) extends Checked[A, R] {
    
    -// final case class KayoAsOkay[+A, +S](kayo: Kayo[A, S]) extends Checked[A, S] {
    
    -final case class Kayo[+O, +K](k: K) extends Checked[O, K] {
    +final case class Kayo[+A, +R](r: R) extends Checked[A, R] {
    
    -final case class OkayAsKayo[+A, +S](okay: Okay[A, S]) extends Checked[A, S] {
    +final case class OkayAsKayo[+A, +R](okay: Okay[A, R]) extends Checked[A, R] {

--- src/main/scala/Checked.scala
+++ src/main/scala/Checked.scala
@@ -1,30 +1,39 @@
 package org.lafros.scala
 
-sealed abstract class Checked[+A, +S] {
+sealed abstract class Checked[+A, +R] {
   def isOkay: Boolean
-  //def get: A
+  def get: A = this match {
+    case Okay(a) => a
+    case Kayo(_) => throw new NoSuchElementException("Kayo.get")
+    case OkayAsKayo(_) => throw new NoSuchElementException("OkayAsKayo.get")
+  }
+  def reason: R = this match {
+    case Okay(_) => throw new NoSuchElementException("Okay.reason")
+    case Kayo(r) => r
+    case OkayAsKayo(_) => throw new NoSuchElementException("OkayAsKayo.reason")
+  }
   def getOrElse[B >: A](b: => B) = this match {
     case Okay(a) => a
     case Kayo(_) => b
     case OkayAsKayo(_) => b
   }
-  def map[B](f: A => B): Checked[B, S] = this match {
+  def map[B](f: A => B): Checked[B, R] = this match {
     case Okay(a) => Okay(f(a))
-    case Kayo(b) => Kayo(b)
+    case Kayo(r) => Kayo(r)
     case OkayAsKayo(Okay(a)) => OkayAsKayo(Okay(f(a)))
   }
-  def flatMap[B, T >: S](f: A => Checked[B, T]): Checked[B, T] = this match {
+  def flatMap[B, S >: R](f: A => Checked[B, S]): Checked[B, S] = this match {
     case Okay(a) => f(a)
-    case Kayo(b) => Kayo(b)
+    case Kayo(s) => Kayo(s)
     case OkayAsKayo(Okay(a)) => f(a) match {
       case okay @ Okay(_) => OkayAsKayo(okay)
       case kayo @ Kayo(_) => kayo
       case kayo @ OkayAsKayo(_) => kayo
     }
   }
-  def withFilter(p: A => Boolean): Checked[A, S] = this match {
+  def withFilter(p: A => Boolean): Checked[A, R] = this match {
     case okay @ Okay(a) => if (p(a)) okay else OkayAsKayo(okay)
-    case kayo @ Kayo(b) => kayo
+    case kayo @ Kayo(_) => kayo
     case kayo @ OkayAsKayo(_) => kayo
   }
   def foreach[B](f: A => B): Unit = this match {
@@ -34,18 +43,14 @@ sealed abstract class Checked[+A, +S] {
   }
 }
 
-final case class Okay[+A, +S](a: A) extends Checked[A, S] {
+final case class Okay[+A, +R](a: A) extends Checked[A, R] {
   def isOkay = true
 }
 
-// final case class KayoAsOkay[+A, +S](kayo: Kayo[A, S]) extends Checked[A, S] {
-//   def okay = true
-// }
-
-final case class Kayo[+O, +K](k: K) extends Checked[O, K] {
+final case class Kayo[+A, +R](r: R) extends Checked[A, R] {
   def isOkay = false
 }
 
-final case class OkayAsKayo[+A, +S](okay: Okay[A, S]) extends Checked[A, S] {
+final case class OkayAsKayo[+A, +R](okay: Okay[A, R]) extends Checked[A, R] {
   def isOkay = false
 }
