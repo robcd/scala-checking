e3c4ce7645752508f19807b4b7ba60438ec71ff4
Rob Dickens <rob.dickens@lafros.com>
2012-05-30 17:12:23 +0100

    O=>A K=>S okay=>isOkay +map +foreach //KayoAsOkay

--- src/main/scala/Checked.scala
+++ src/main/scala/Checked.scala
@@ -1,21 +1,31 @@
 package org.lafros.scala
 
-sealed abstract class Checked[+O, +K] {
-  def okay: Boolean
+sealed abstract class Checked[+A, +S] {
+  def isOkay: Boolean
+  def map[B](f: A => B): Checked[B, S] = this match {
+    case Okay(a) => Okay(f(a))
+    case Kayo(b) => Kayo(b)
+    case OkayAsKayo(Okay(a)) => OkayAsKayo(Okay(f(a)))
+  }
+  def foreach[B](f: A => B): Unit = this match {
+    case Okay(a) => f(a)
+    case Kayo(_) =>
+    case OkayAsKayo(_) =>
+  }
 }
 
-final case class Okay[+O, +K](o: O) extends Checked[O, K] {
-  def okay = true
+final case class Okay[+A, +S](a: A) extends Checked[A, S] {
+  def isOkay = true
 }
 
-final case class KayoAsOkay[+O, +K](k: K) extends Checked[O, K] {
-  def okay = true
-}
+// final case class KayoAsOkay[+A, +S](kayo: Kayo[A, S]) extends Checked[A, S] {
+//   def okay = true
+// }
 
 final case class Kayo[+O, +K](k: K) extends Checked[O, K] {
-  def okay = false
+  def isOkay = false
 }
 
-final case class OkayAsKayo[+O, +K](o: O) extends Checked[O, K] {
-  def okay = false
+final case class OkayAsKayo[+A, +S](okay: Okay[A, S]) extends Checked[A, S] {
+  def isOkay = false
 }
