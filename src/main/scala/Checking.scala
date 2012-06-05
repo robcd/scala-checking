11e523acaf90d98c545037d4312d0b85ffa96784
Rob Dickens <rob.dickens@lafros.com>
2012-06-05 13:26:29 +0100

    Checking.scala: doc-comments added

--- src/main/scala/Checking.scala
+++ src/main/scala/Checking.scala
@@ -1,12 +1,31 @@
 package org.lafros.scala
-
+/**
+ * Mix-in alternative to importing Checking._, when wishing to use the nested class, Checked.
+ */
 trait Checking {
+  /**
+   * Yet another 'Right-biased Either' candidate.
+   *
+   * For use as the return type, in place of A, wherever an exception might otherwise be thrown
+   * and a more 'functional' style is preferred.
+   *
+   * The value of A to be returned is returned wrapped in an instance of one of only two
+   * concrete subclasses of Checked, depending on whether or not an exception arose.
+   *
+   * If there was no exception, this will be the Okay subclass, otherwise, the Reason one.
+   */
   sealed abstract class Checked[+A, +R] {
     def isOkay: Boolean
+    /**
+     * throws a NoSuchElementException if this is a Reason.
+     */
     def get: A = this match {
       case Okay(a) => a
       case Reason(_) => throw new NoSuchElementException("Reason.get")
     }
+    /**
+     * throws a NoSuchElementException if this is an Okay.
+     */
     def reason: R = this match {
       case Okay(_) => throw new NoSuchElementException("Okay.reason")
       case Reason(r) => r
@@ -29,22 +48,38 @@ trait Checking {
     }
     def toOption: Option[A] = if (isOkay) Some(get) else None
   }
-
+  /**
+   * see Checked.
+   */
   final case class Okay[+A, +R](a: A) extends Checked[A, R] {
     def isOkay = true
   }
-
+  /**
+   * see Checked.
+   */
   final case class Reason[+A, +R](r: R) extends Checked[A, R] {
     def isOkay = false
   }
-
+  /**
+   * type to which some T is converted by the implicit conversion, any2CheckedLift.
+   */
   abstract class CheckedLift[T] {
     def toOkay  [R]:   Okay[T, R]
     def toReason[A]: Reason[A, T]
+    /**
+     * ff as in fail-fast: applies checks to the target T, in a fail-fast manner (stopping after
+     * the first Reason is encountered).
+     */
     def ff [R](checks: (T => Checked[T, R])*): Checked[T, R]
+    /**
+     * fs as in fail-slowly: applies checks to the target T, in a fail-slowly manner (continuing
+     * after the first Reason is encounted).
+     */
     def fs [R](checks: (T => Checked[T, R])*): Checked[T, Iterable[R]]
   }
-
+  /**
+   * See CheckedLift.
+   */
   implicit def any2CheckedLift[T](any: T): CheckedLift[T] = new CheckedLift[T] {
     def toOkay  [R] = Okay[T, R](any)
     def toReason[A] = Reason[A, T](any)
@@ -66,12 +101,15 @@ trait Checking {
       if (rs.isEmpty) Okay(any) else Reason(rs)
     }
   }
-
+  /**
+   * the type of value that will be wrapped in a Reason, that only needs to be supplied when
+   * using the implicitly-supplied operator, <*>.
+   */
   type R
 
   trait FailFastAppFunct[A, B] {
     /**
-     * handles a fail-slowly result, where Result contains an Iterable. */
+     * handles a fail-slowly result where Reason contains an Iterable. */
     def <*>[Rs](checked: Checked[A, Rs])(implicit ev: Rs <:< Iterable[R]): Checked[B, R]
     /**
      * confers fail-fast applicative-functor status: lets you apply a FunctionN to N values in N
@@ -80,7 +118,7 @@ trait Checking {
   }
   trait FailSlowlyAppFunct[A, B] {
     /**
-     * handles a fail-slow result, where Left contains a List. */
+     * handles a fail-slowly result where Reason contains an Iterable. */
     def <*>[Rs](checked: Checked[A, Rs])(implicit ev: Rs <:< Iterable[R]): Checked[B, Iterable[R]]
     /**
      * confers fail-slowly applicative-functor status: lets you apply a FunctionN to N values
@@ -88,10 +126,12 @@ trait Checking {
     def <*>(checked: Checked[A, R]): Checked[B, Iterable[R]]
   }
   /**
-   * lifts f, which must be curried, into an Okay, for fail-fast application. */
+   * lifts f, which must be curried, into an Okay, for fail-fast application (stopping after
+   * the first Reason is encountered). */
   def ff[A, B](f: A => B) = Okay[A => B, R](f)
   /**
-   * as above, but for fail-slowly application. */
+   * as above, but for fail-slowly application (continuing after the first Reason is
+   * encountered). */
   def fs[A, B](f: A => B) = Okay[A => B, Iterable[R]](f)
 
   implicit def checkedOfFun2ffap[A, B](f: Checked[A => B, R]): FailFastAppFunct[A, B] =
@@ -123,7 +163,10 @@ trait Checking {
       }
     }
 }
-
+/**
+ * companion object provided as a convenience, that may be imported, instead of mixing in the
+ * trait, and having R set to String.
+ */
 object Checking extends Checking {
-  type L = String
+  type R = String
 }
