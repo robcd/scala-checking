c1e0d039cf0d880d909f8e90c560077f808251a9
Rob Dickens <rob.dickens@lafros.com>
2012-06-09 19:39:54 +0100

    testMusicians: comment added showing input and resulting output

--- src/test/scala/testMusicians.scala
+++ src/test/scala/testMusicians.scala
@@ -1,3 +1,25 @@
 object testMusicians extends App with Musicians {
   readFromFile("musicians.db") foreach println
+
+// The following input...
+//
+//   Stevie Wonder,            pop, voice, piano
+//     Chick Corea,           jazz, piano
+// Michael Brecker,           jazz, sax
+//   Randy Brecker,           jazz, trumpet
+//  Charlie Parker,           jazz, sax
+//       Thor Chuh, easy listening, pan pipes
+//    Des O'Connor, easy listening, voice
+//     Rolf Harris, easy listening, voice, didgeridoo
+//
+// ...produces the following output:
+//
+// Okay(Musician(Stevie Wonder,pop,ArrayBuffer(voice, piano)))
+// Okay(Musician(Chick Corea,jazz,ArrayBuffer(piano)))
+// Okay(Musician(Michael Brecker,jazz,ArrayBuffer(sax)))
+// Okay(Musician(Randy Brecker,jazz,ArrayBuffer(trumpet)))
+// Okay(Musician(Charlie Parker,jazz,ArrayBuffer(sax)))
+// Reason(List(easy listening, includes pan pipes))
+// Reason(List(easy listening))
+// Reason(List(easy listening, unrecognised instrument: didgeridoo))
 }
